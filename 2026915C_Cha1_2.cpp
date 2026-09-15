d1f3e1519568563be6d3c97100c2862239e9e1ca
linfon18 <mczlf@qq.com>
2026-09-15 20:57:50 +0800

    Add learning README and C practice

---
+++ 2026915C_Cha1_2.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<Windows.h>
+#define HYW "BBBB"
+#define B 393
+#define N 444
+#define M 494
+#define A 523
+
+int main(void)
+{
+	printf("Hello World\n");
+	Beep(262,500);
+	int ac;
+	printf(HYW);
+
+	Beep(B, 400);
+	Beep(B, 400);
+	Beep(N, 500);
+	Beep(A, 600);
+	Beep(M, 600);
+	return 0;
+	
+}
+
+//学习记录:
+//第一章-第二章2.1 P13
